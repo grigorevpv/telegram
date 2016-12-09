b8f749b0c027070877a9d03ee32c1f88e2a3f6d9
grigorevpv <grigorevpv@gmail.com>
2016-12-08 17:20:36 -0800

    add message_function

--- cmake-build-debug/CMakeFiles/telegram.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/telegram.dir/cmake_clean.cmake
@@ -2,14 +2,11 @@ file(REMOVE_RECURSE
   "CMakeFiles/telegram.dir/main.cpp.o"
   "CMakeFiles/telegram.dir/lib/Users.cpp.o"
   "CMakeFiles/telegram.dir/lib/Cards.cpp.o"
-  "CMakeFiles/telegram.dir/lib/config.c.o"
-  "CMakeFiles/telegram.dir/lib/rc522.c.o"
-  "CMakeFiles/telegram.dir/lib/rfid.c.o"
   "telegram.pdb"
   "telegram"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
+foreach(lang CXX)
   include(CMakeFiles/telegram.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
